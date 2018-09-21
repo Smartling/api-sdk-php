68103d0ac06006747272afbca7417fb71e826424
Pavlo Ratushnyi <pratushnyi@smartling.com>
2018-09-21 14:41:24 +0300

    Removed unused state

--- src/TranslationRequests/Params/TranslationSubmissionStates.php
+++ src/TranslationRequests/Params/TranslationSubmissionStates.php
@@ -11,7 +11,6 @@ class TranslationSubmissionStates
     const STATE_NEW = 'New';
     const STATE_IN_PROGRESS = 'In Progress';
     const STATE_TRANSLATED = 'Translated';
-    const STATE_CHANGED = 'Changed';
     const STATE_FAILED = 'Failed';
     const STATE_DELETED = 'Deleted';
     const STATE_COMPLETED = 'Completed';
@@ -23,7 +22,6 @@ class TranslationSubmissionStates
         self::STATE_NEW,
         self::STATE_IN_PROGRESS,
         self::STATE_TRANSLATED,
-        self::STATE_CHANGED,
         self::STATE_FAILED,
         self::STATE_DELETED,
         self::STATE_COMPLETED,
