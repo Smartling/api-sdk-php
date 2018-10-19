ed26513b281e2e97afdcd4bf3b39d69cb32a7325
Loparev <pavel.loparev.brdm2@gmail.com>
2018-10-19 11:00:59 +0300

    Added "contentFileUri" parameter for /upload-and-match-async and /match/async calls

--- src/Context/Params/UploadContextParameters.php
+++ src/Context/Params/UploadContextParameters.php
@@ -8,12 +8,27 @@ use Smartling\Parameters\BaseParameters;
  * Class UploadContextParameters
  * @package Context\Params
  */
-class UploadContextParameters extends BaseParameters
+class UploadContextParameters extends BaseParameters implements ContextParametersInterface
 {
 
+    /**
+     * @param $contextFileUri
+     *
+     * @deprecated since version 3.5.0, to be removed in 4.0.0.
+     * Use UploadContextParameters::setContent() instead.
+     */
     public function setContextFileUri($contextFileUri)
     {
         $this->params['content'] = $contextFileUri;
+    }
+
+    public function setContent($contextFileUri)
+    {
+        $this->params['content'] = $contextFileUri;
+    }
+
+    public function setContentFileUri($contentFileUri) {
+        $this->params['contentFileUri'] = $contentFileUri;
     }
 
     public function setName($name) {
