769094c693664efec8226e87743106bd5df4d057
Loparev <pavel.loparev.brdm2@gmail.com>
2019-05-02 22:16:42 +0300

    Increased sdk version: 3.6.4 -> 3.6.5

--- tests/unit/FileApiTest.php
+++ tests/unit/FileApiTest.php
@@ -120,7 +120,7 @@ class FileApiTest extends ApiTestAbstract
                     ],
                     [
                         'name' => 'smartling.client_lib_id',
-                        'contents' => '{"client":"smartling-api-sdk-php","version":"3.6.4"}',
+                        'contents' => '{"client":"smartling-api-sdk-php","version":"3.6.5"}',
                     ],
                     [
                         'name' => 'localeIdsToAuthorize[]',
