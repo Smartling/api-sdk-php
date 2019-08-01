297999375da0a4b4984bc0a4643e23ae775e0c94
Loparev <pavel.loparev.brdm2@gmail.com>
2019-08-01 13:05:24 +0300

    Added sort options to search translation request parameters

--- tests/unit/FileApiTest.php
+++ tests/unit/FileApiTest.php
@@ -120,7 +120,7 @@ class FileApiTest extends ApiTestAbstract
                     ],
                     [
                         'name' => 'smartling.client_lib_id',
-                        'contents' => '{"client":"smartling-api-sdk-php","version":"3.6.7"}',
+                        'contents' => '{"client":"smartling-api-sdk-php","version":"3.7.0"}',
                     ],
                     [
                         'name' => 'localeIdsToAuthorize[]',
