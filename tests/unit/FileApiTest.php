d258be77f35d9fb0e09396de8299067e56031fa9
Loparev <pavel.loparev.brdm2@gmail.com>
2020-01-28 13:07:06 +0200

    Increased version to 3.8.0

--- tests/unit/FileApiTest.php
+++ tests/unit/FileApiTest.php
@@ -120,7 +120,7 @@ class FileApiTest extends ApiTestAbstract
                     ],
                     [
                         'name' => 'smartling.client_lib_id',
-                        'contents' => '{"client":"smartling-api-sdk-php","version":"3.7.0"}',
+                        'contents' => '{"client":"smartling-api-sdk-php","version":"3.8.0"}',
                     ],
                     [
                         'name' => 'localeIdsToAuthorize[]',
