1e97412f31f67fcf51b51508f87ce8d1e3a1c31a
Loparev <pavel.loparev.brdm2@gmail.com>
2022-05-17 19:02:31 +0300

    Added listJobFiles method to JobsAPI client

--- src/Jobs/JobsApi.php
+++ src/Jobs/JobsApi.php
@@ -10,6 +10,7 @@ use Smartling\Jobs\Params\AddFileToJobParameters;
 use Smartling\Jobs\Params\AddLocaleToJobParameters;
 use Smartling\Jobs\Params\CancelJobParameters;
 use Smartling\Jobs\Params\CreateJobParameters;
+use Smartling\Jobs\Params\ListJobFilesParameters;
 use Smartling\Jobs\Params\ListJobsParameters;
 use Smartling\Jobs\Params\SearchJobsParameters;
 use Smartling\Jobs\Params\UpdateJobParameters;
@@ -229,6 +230,22 @@ class JobsApi extends BaseApiAbstract
     {
         $endpoint = \vsprintf('jobs/%s/processes/%s', [$jobId, $processId]);
         $requestData = $this->getDefaultRequestData('query', []);
+
+        return $this->sendRequest($endpoint, $requestData, self::HTTP_METHOD_GET);
+    }
+
+    /**
+     * Returns files within the job.
+     *
+     * @param $jobId
+     * @param ListJobFilesParameters $parameters
+     * @return mixed
+     * @throws SmartlingApiException
+     */
+    public function listJobFiles($jobId, ListJobFilesParameters $parameters)
+    {
+        $endpoint = \vsprintf('jobs/%s/files', [$jobId]);
+        $requestData = $this->getDefaultRequestData('query', $parameters->exportToArray());
 
         return $this->sendRequest($endpoint, $requestData, self::HTTP_METHOD_GET);
     }
