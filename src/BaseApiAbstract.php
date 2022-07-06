0bb269aa425e3274d2088f39ce7c93721b07653d
Loparev <pavel.loparev.brdm2@gmail.com>
2022-07-06 11:36:55 +0300

    Bumped version

--- src/BaseApiAbstract.php
+++ src/BaseApiAbstract.php
@@ -22,7 +22,7 @@ abstract class BaseApiAbstract
 {
     const CLIENT_LIB_ID_SDK = 'smartling-api-sdk-php';
 
-    const CLIENT_LIB_ID_VERSION = '4.0.5';
+    const CLIENT_LIB_ID_VERSION = '4.0.6';
 
     const CLIENT_USER_AGENT_EXTENSION = '(no extensions)';
 
